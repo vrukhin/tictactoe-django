3add862f5d686b193b372676bc8fb3e329708ad4
Pavel Vrukhin <vrukhin@protonmail.com>
2022-05-13 13:46:36 +0300

    Добавлены стили

--- tictac/forms.py
+++ tictac/forms.py
@@ -6,3 +6,12 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = ['room_name', 'room_description']
+        labels = {
+            'room_name': 'Название комнаты',
+            'room_description': 'Описание',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RoomForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'text-field'
